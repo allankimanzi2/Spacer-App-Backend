0ae1900a4edb87d3d92eef70824a7d2686aed6da
abdullahiabdi <cabudicali218@gmail.com>
2024-05-19 22:14:33 +0300

    done

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,15 @@ from app import jwt
 from usermodel import User
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    user = User.query.filter_by(username=username).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message='Invalid username or password'), 401
